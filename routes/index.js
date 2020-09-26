6c0b117c1000558125b0b5f4e89861b61e713999
alegleason <A01703013@itesm.mx>
2020-09-26 01:38:46 -0500

    Finish beta version

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,20 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.get('/test', (req, res) => {
-    res.render('test')
+router.get('/about-me', (req, res) => {
+    res.render('about')
+})
+
+router.get('/my-projects', (req, res) => {
+    res.render('projects')
+})
+
+router.get('/my-resume', (req, res) => {
+    res.render('resume')
+})
+
+router.get('/lgbtq-involvement', (req, res) => {
+    res.render('lgbtq')
 })
 
 module.exports = router
